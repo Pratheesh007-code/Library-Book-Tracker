9b9f13da74481bb8010f23c5a4533b5520b346a4
Pratheesh007-code <pratheesh80722@gmail.com>
2026-08-30 09:51:48 +0530

    Complete Week 5 library book tracker

--- week 5/library_book_tracker.py
+++ week 5/library_book_tracker.py
@@ -1,7 +1,7 @@
 books = {}
 
 while True:
-    print("\n1.Add  2.View  3.Check  4.Issue  5.Return  6.Exit")
+    print("\n1.Add 2.View 3.Check 4.Issue 5.Return 6.Search 7.Update 8.Delete 9.Exit")
     ch = input("Choice: ")
 
     if ch == "1":
@@ -49,4 +49,31 @@ while True:
             print("Book Not Found")
 
     elif ch == "6":
+        key = input("Search: ").lower()
+    
+        for i, b in books.items():
+            if key in b[0].lower() or key in b[1].lower():
+                print(i, b)
+
+    elif ch == "7":
+        i = input("Book ID: ")
+    
+        if i in books:
+            books[i][0] = input("New Title: ")
+            books[i][1] = input("New Author: ")
+            print("Book Updated")
+        else:
+            print("Book Not Found")
+
+    
+    elif ch == "8":
+        i = input("Book ID: ")
+    
+        if i in books:
+            del books[i]
+            print("Book Deleted")
+        else:
+            print("Book Not Found")
+
+    elif ch == "9":
         break
